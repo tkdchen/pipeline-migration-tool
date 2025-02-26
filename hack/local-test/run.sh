c205f44991673914a1375f87f80194b9bf71ac76
Chenxiong Qi <cqi@redhat.com>
2025-02-26 14:22:06 +0800

    first commit for testing

--- hack/local-test/run.sh
+++ hack/local-test/run.sh
@@ -1,200 +1,158 @@
 #!/usr/bin/env bash
 
-set -e
-set -o pipefail
+# BUILD_DEFS_REPO=~cqi/code/konflux/build-definitions QUAY_NAMESPACE=mytestworkload/build-definitions-test bash -x ./hack/local-test/run.sh
 
-# podman login quay.io
-# cp "${XDG_RUNTIME_DIR}/containers/auth.json" ~/.docker/config.json
-REGISTRY_AUTH_JSON=${REGISTRY_AUTH_JSON:-"$HOME/.docker/config.json"}
+set -euo pipefail
 
-MIGRATION_ANNOTATION=dev.konflux-ci.task.migration
-MIGRATION_ARTIFACT_TYPE=text/x-shellscript
+: "${BUILD_DEFS_REPO:?Missing repository path of build-definitions}"
+: "${QUAY_NAMESPACE:?Missing QUAY_NAMESPACE. build-and-push.sh uses it to push task bundles}"
 
-TASK_NAMES=(clone tests)
+export QUAY_NAMESPACE
 
-IMAGE_PREFIX=${IMAGE_NS:-quay.io/mytestworkload/pmt-playground}
+declare -r DEFAULT_RECIPES="clone 0.2 -
+clone 0.2 M
+clone 0.2 -
+clone 0.2 M
+clone 0.2 M
+lint 0.2 -
+lint 0.2 M
+lint 0.2 M
+lint 0.2 M
+lint 0.2 -
+"
 
-if [[ ! -e README.md ]]; then
-    echo "error: script must run from the root of the repository." >&2
-    exit 1
-fi
+: "${RECIPES:=$DEFAULT_RECIPES}"
 
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$SCRIPTDIR" || exit 1
 
-retry() {
-  local status
-  local retry=0
-  local -r interval=${RETRY_INTERVAL:-5}
-  local -r max_retries=5
-  while true; do
-    "$@" && break
-    status=$?
-    ((retry+=1))
-    if [ $retry -gt $max_retries ]; then
-      return $status
-    fi
-    echo "Waiting for a while, then retry ..."
-    sleep "$interval"
-  done
-}
-
-
-generate_sha1_hash() {
-  echo "$1 $(date)" | sha1sum - | (read -r checksum _; echo "$checksum")
-}
-
-
-declare -a clone_upgrades_data=()
-declare -a tests_upgrades_data=()
+declare -r DEFINITIONS_DIR="${SCRIPTDIR}/definitions"
 
 
 build_and_push() {
-  local -r task_name="$1"
-  local -r task_file="hack/local-test/task/${task_name}/${task_name}.yaml"
-  local -r image="${IMAGE_PREFIX}/task-${task_name}"
-  local -a task_versions=()
-
-  if [[ "$task_name" == "clone" ]]; then
-    task_versions+=(0.1.0 0.2.0 0.2.1 0.2.4 0.3.0 0.4.0)
-  elif [[ "$task_name" == "tests" ]]; then
-    task_versions+=(0.1.0 0.2.0 0.2.1 0.3.0 0.3.2)
-  else
-    echo "error: script does not work for task name $task_name" >&2
-    return 1
-  fi
-
-  for task_version in "${task_versions[@]}"
-  do
-    echo "info: handle task version ${task_version}"
-    modified_task_file="/tmp/${task_file##*/}-modified"
-    migration_file="hack/local-test/task/${task_name}/migrations/${task_version}.sh"
-
-    yq ".metadata.labels.\"app.kubernetes.io/version\" |= \"${task_version}\"" "$task_file" | \
-      yq ".metadata.labels.\"dev.konflux-ci/updated-datetime\" |= \"$(date)\"" \
-      >"$modified_task_file"
-    annotations=()
-    if [[ -e "$migration_file" ]]; then
-      echo "info: task $task_name has a migration $migration_file"
-      annotations+=(--annotate "${MIGRATION_ANNOTATION}=true")
-    fi
-
-    sha1_hash=$(generate_sha1_hash "$task_version")
-    image_tag="${task_version%.*}-${sha1_hash}"
-    image_with_tag="$image:$image_tag"
-    echo "info: push task bundle $image_with_tag"
-    output=$(retry tkn bundle push "${image_with_tag}" -f "$modified_task_file" "${annotations[@]}")
-    echo "$output"
-    bundle_ref=$(echo "$output" | grep "sha256:[0-9a-f]\+" | cut -d' ' -f5)
-
-    if [[ -e "$migration_file" ]]; then
-      echo "info: attach migration file ${migration_file} to task bundle ${bundle_ref}"
-      pushd "${migration_file%/*}"
-      retry oras attach --registry-config "$REGISTRY_AUTH_JSON" \
-        --annotation "${MIGRATION_ANNOTATION}=true" \
-        --artifact-type "$MIGRATION_ARTIFACT_TYPE" \
-        --distribution-spec v1.1-referrers-tag \
-        "$bundle_ref" \
-        "${migration_file##*/}"
-      popd
-    fi
-
-    if [[ "$task_name" == "clone" ]]; then
-      clone_upgrades_data+=("$image $image_tag ${bundle_ref##*@}")
-    elif [[ "$task_name" == "tests" ]]; then
-      tests_upgrades_data+=("$image $image_tag ${bundle_ref##*@}")
-    fi
-  done
+    HERE="$DEFINITIONS_DIR" \
+    SKIP_INSTALL=1 \
+    SKIP_BUILD=1 \
+    BUILD_TAG=$(git rev-parse HEAD) \
+    OUTPUT_TASK_BUNDLE_LIST=/tmp/pmt-test-task-bundle-list \
+    OUTPUT_PIPELINE_BUNDLE_LIST=/tmp/pmt-test-pipeline-bundle-list \
+        bash "${BUILD_DEFS_REPO}/hack/build-and-push.sh"
 }
 
+bump_task_version() {
+    local -r task_file=$1
+    local patch
+    local new_version
 
-remove_task_bundles() {
-  local image
-  local -a tags
-  for task_name in "${TASK_NAMES[@]}"; do
-    image="${IMAGE_PREFIX}/task-${task_name}"
-    echo "info: listing tags from image repository ${image}"
-    mapfile -t tags < <(skopeo list-tags "docker://${image}" | jq -r '.Tags[]')
-    for tag_name in "${tags[@]}"; do
-      echo "info: remove image tag ${image}:${tag_name}"
-      skopeo delete "docker://${image}:${tag_name}"
-    done
-  done
+    IFS=. read -r major minor patch < <(
+        yq '.metadata.labels."app.kubernetes.io/version"' "$task_file"
+    )
+    patch=$((patch+1))
+    new_version="${major}.${minor}.${patch}"
+    # app.kubernetes.io/version: "0.3.1"
+    sed -i "s|^\( \+app\.kubernetes\.io/version\): .\+$|\1: \"${new_version}\"|" "$task_file"
+    echo "$new_version"
 }
 
+create_migration() {
+    local -r task_file=$1
+    local -r new_version=$2
+    local -r versioned_dir=${task_file%/*}
 
-make_renovate_upgrades() {
-  local upgrade_from upgrade_to
-  local n
-  local dep_name current_value current_digest new_value new_digest
+    local -r migration_dir="${versioned_dir}/migrations"
+    [ -e "$migration_dir" ] || mkdir -p "$migration_dir"
 
-  local -r data_file="/tmp/pmt-upgrades-data.txt"
+    local -r migration_file="${migration_dir}/${new_version}.sh"
 
-  echo -n "[" | tee "$data_file"
+    cat >"$migration_file" <<EOF
+#!/usr/bin/env bash
+set -euo pipefail
+declare -r pipeline_file=\${1:?Missing pipeline file}
+EOF
+    echo "$migration_file"
+}
 
-  upgrade_from=${clone_upgrades_data[0]}
-  dep_name=$(cut -d' ' -f1 <<<"$upgrade_from")
-  current_value=$(cut -d' ' -f2 <<<"$upgrade_from")
-  current_digest=$(cut -d' ' -f3 <<<"$upgrade_from")
-  n=${#clone_upgrades_data[@]}
-  n=$((n-1))
-  upgrade_to=${clone_upgrades_data[$n]}
-  dep_name=$(cut -d' ' -f1 <<<"$upgrade_to")
-  new_value=$(cut -d' ' -f2 <<<"$upgrade_to")
-  new_digest=$(cut -d' ' -f3 <<<"$upgrade_to")
+bump_task_version_and_commit() {
+    local -r task_name=$1 task_version=$2
+    local -r task_file="task/${task_name}/${task_version}/${task_name}.yaml"
+    bump_task_version "$task_file"
+    git add "$task_file"
+    git commit -m "bump version of task $task_name"
+}
 
+update_task_with_migration() {
+    local -r task_name=$1 task_version=$2
+    local -r task_file="task/${task_name}/${task_version}/${task_name}.yaml"
+    local new_version migration_file
+    new_version=$(bump_task_version "$task_file")
+    migration_file=$(create_migration "$task_file" "$new_version")
+    git add "$task_file" "$migration_file"
+    git commit -m "Create a migration for task $task_name"
+    build_and_push
+}
+
+image_to_renovate_upgrade() {
+    local -r old=$1 new=$2
+    local dep_name=${old%@*}
+    dep_name=${dep_name%:*}
+    current_value=${old%@*}; current_value=${current_value##*:}
+    current_digest=${old#*@}
+    new_value=${new%@*}; new_value=${new_value##*:}
+    new_digest=${new#*@}
     echo "{
-\"depName\": \"${dep_name}\",
-\"currentValue\": \"${current_value}\",
-\"currentDigest\": \"${current_digest}\",
-\"newValue\": \"${new_value}\",
-\"newDigest\": \"${new_digest}\",
-\"packageFile\": \"hack/local-test/ci-pipeline.yaml\",
-\"parentDir\": \"local-tests\",
-\"depTypes\": [\"tekton-bundle\"]
-}," | tee -a "$data_file"
+    \"depName\": \"${dep_name}\",
+    \"currentValue\": \"${current_value}\",
+    \"currentDigest\": \"${current_digest}\",
+    \"newValue\": \"${new_value}\",
+    \"newDigest\": \"${new_digest}\",
+    \"packageFile\": \".tekton/ci-pipeline.yaml\",
+    \"parentDir\": \".tekton\",
+    \"depTypes\": [\"tekton-bundle\"]
+}
+"
+}
 
-  upgrade_from=${tests_upgrades_data[0]}
-  dep_name=$(cut -d' ' -f1 <<<"$upgrade_from")
-  current_value=$(cut -d' ' -f2 <<<"$upgrade_from")
-  current_digest=$(cut -d' ' -f3 <<<"$upgrade_from")
-  n=${#tests_upgrades_data[@]}
-  n=$((n-1))
-  upgrade_to=${tests_upgrades_data[$n]}
-  dep_name=$(cut -d' ' -f1 <<<"$upgrade_to")
-  new_value=$(cut -d' ' -f2 <<<"$upgrade_to")
-  new_digest=$(cut -d' ' -f3 <<<"$upgrade_to")
+get_newest_oldest_image() {
+    local -r task_name=$1
+    curl -sL "https://quay.io/api/v1/repository/${QUAY_NAMESPACE}/task-${task_name}/tag/?onlyActiveTags=true&limit=30" | \
+    jq -r '.tags[] | .name + " " + .manifest_digest' | \
+        while read -r tag_name manifest_digest; do
+            echo "quay.io/${QUAY_NAMESPACE}/task-clone:${tag_name%-*}@${manifest_digest}"
+        done | \
+            unique | \
+            sed -n '1p; $p' | \
+            tr '\n' ' ' # the newest one, then the oldest one
+}
 
-    echo -n "{
-\"depName\": \"${dep_name}\",
-\"currentValue\": \"${current_value}\",
-\"currentDigest\": \"${current_digest}\",
-\"newValue\": \"${new_value}\",
-\"newDigest\": \"${new_digest}\",
-\"packageFile\": \"hack/local-test/ci-pipeline.yaml\",
-\"parentDir\": \"local-tests\",
-\"depTypes\": [\"tekton-bundle\"]
-}" | tee -a "$data_file"
-
-  echo "]" | tee -a "$data_file"
-
-  echo
-  echo "Sample Renovate upgrades is written into $data_file"
+produce_bundles() {
+    cd "$DEFINITIONS_DIR" || return 1
+    while read -r task_name task_version create_migration
+    do
+        echo "Handle recipe: task $task_name, version $task_version, create a migration? $create_migration"
+        if [ "$create_migration" == "M" ]; then
+            update_task_with_migration "$task_name" "$task_version"
+        else
+            bump_task_version_and_commit "$task_name" "$task_version"
+        fi
+    done <<<"$RECIPES"
 }
 
 
-main() {
-  local -r op=$1
-  if [[ "$op" == "build-and-push" ]]; then
-    for task_name in "${TASK_NAMES[@]}"; do
-      echo "info: build and push task $task_name"
-      build_and_push "$task_name"
-    done
-    make_renovate_upgrades
-  elif [[ "$op" == "remove-task-bundles" ]]; then
-    remove_task_bundles
-  else
-    echo "info: I don't know what to do for operation $op." >&2
-    return 1
-  fi
-}
+###### do the job ######
 
-main "$@"
+produce_bundles
+
+{
+    echo "["
+    read -r newest oldest < <(get_newest_oldest_image clone)
+    image_to_renovate_upgrade "$oldest" "$newest"
+    echo ","
+    read -r newest oldest < <(get_newest_oldest_image lint)
+    image_to_renovate_upgrade "$oldest" "$newest"
+    echo "]"
+} >/tmp/pmt-test-upgrades.txt
+
+echo "Then,
+cd app
+pipeline-migration-tool -u '$(cat /tmp/pmt-test-upgrades.txt)'
+"
